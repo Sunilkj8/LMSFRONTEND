46727150b2a955c0117ecba7b49f9784dbdd6911
Sunilkj8 <sunil2003jakhar@gmail.com>
2025-02-08 07:11:51 +0530

    Marquee text added

--- src/components/Landing page/singleCategory.jsx
+++ src/components/Landing page/singleCategory.jsx
@@ -14,13 +14,13 @@ const SingleCategory = ({ image, title }) => {
           window.scroll(0, 0);
         }, 500);
       }}
-      className="relative h-[55vh] cursor-pointer flex justify-center rounded-md    bg-[#000000d0]   items-center   "
+      className="relative h-[55vh] cursor-pointer flex justify-center  rounded-3xl shadow-inner    bg-[#000000d0]   items-center   "
     >
       <motion.div
         animate={{ opacity: reveal ? 0.3 : 0.7 }}
         className="absolute w-full flex justify-center items-center h-full opacity-55  rounded-3xl "
       >
-        <img src={image} className="  w-full h-full" alt="" />
+        <img src={image} className="  rounded-3xl  w-[100%] h-[100%]" alt="" />
       </motion.div>{" "}
       <motion.div
         onMouseOver={() => {
@@ -30,7 +30,7 @@ const SingleCategory = ({ image, title }) => {
           setReveal(false);
         }}
         animate={{ opacity: reveal ? 1 : 0.5 }}
-        className="absolute text-white text-center items-center justify-center flex h-full w-full  text-5xl font-extrabold tracking-wide rounded-xl uppercase"
+        className="absolute text-white text-center items-center justify-center flex h-full w-full  text-2xl font-semibold tracking-wide rounded-3xl capitalize"
       >
         {title}
       </motion.div>
